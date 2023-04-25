d222a3441d98ed2ff14d25203ceee7a11850fdd4
Abhimanyu Rathore <68547049+abhi1649@users.noreply.github.com>
2023-04-25 05:31:30 +0530

    Add files via upload

---
+++ gujaratgraph.py
@@ -0,0 +1,70 @@
+from tkinter import *
+from tkinter.ttk import *
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+master = Tk()
+
+master.geometry("2000x2000")
+master.configure(bg='deep sky blue')
+master.title("Gujarat")
+
+def openNewWindow():
+    newWindow = Toplevel(master)
+
+    Label(newWindow,
+          text="").pack()
+    plt.style.use('bmh')
+    df = pd.read_csv('gujarat.csv')
+
+    x = df['Day']
+    y = df['Confirmed']
+
+    plt.xlabel('Day', fontsize=20)
+    plt.ylabel('Confirmed', fontsize=20)
+    plt.bar(x, y)
+    plt.show()
+
+
+def openNewWindow1():
+    newWindow = Toplevel(master)
+
+    Label(newWindow,
+          text="").pack()
+    plt.style.use('bmh')
+    df = pd.read_csv('gujarat.csv')
+
+    x = df['Day']
+    y = df['Confirmed']
+
+    plt.xlabel('Day', fontsize=20)
+    plt.ylabel('Confirmed', fontsize=20)
+    plt.scatter(x, y)
+    plt.show()
+
+
+label = Label(master,
+              text="GRAPHICAL DATA", font=("Times New Roman", 60))
+
+label.pack(pady=10)
+
+photo = PhotoImage(file=r"gr.png")
+photoimage = photo.subsample(4, 6)
+
+Button(master, image=photoimage,
+       compound=LEFT).pack(side=TOP)
+
+
+btn = Button(master,
+             text="BAR GRAPH",
+             command=openNewWindow)
+btn.pack(pady=10)
+
+btn = Button(master, text="SCATTER GRAPH",
+             command=openNewWindow1)
+
+btn.pack(pady=10)
+btn = Button(master, text="Back", command=master.destroy, ).pack()
+master.mainloop()
+mainloop()
